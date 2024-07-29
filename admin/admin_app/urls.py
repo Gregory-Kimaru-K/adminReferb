bdff0b24b179607fd195f12975ba5bb987038ee1
gregorykariara1@gmail.com <gregorykariara1@gmail.com>
2024-07-29 19:38:51 +0300

    login form

--- admin/admin_app/urls.py
+++ admin/admin_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.admin_view, name='admin')
+    path('', views.admin_view, name='admin'),
+    path('login/', views.login_view, name='login')
 ]
--- admin/admin_app/views.py
+++ admin/admin_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def admin_view(request):
     return render(request, 'base.html')
+
+def login_view(request):
+    return render(request, 'registration/login.html')
