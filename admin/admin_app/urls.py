f5ddd895794e4db3c0e62f6ed81eaaa000912837
gregorykariara1@gmail.com <gregorykariara1@gmail.com>
2024-08-07 11:09:05 +0300

    Commitment and consistency

--- admin/admin_app/urls.py
+++ admin/admin_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.admin_view, name='admin'),
-    path('login/', views.login_view, name='login')
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout')
 ]
--- admin/admin_app/views.py
+++ admin/admin_app/views.py
@@ -1,8 +1,34 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required(login_url='/login')
 def admin_view(request):
-    return render(request, 'base.html')
+    context = {
+        'username' : request.user.username
+    }
+    return render(request, 'base.html', context)
 
 def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('user_name')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return JsonResponse({'success' : True, 'redirect_url' : '/'})
+        
+        else:
+            return JsonResponse({'success' : False, 'message' : 'Invalid credentials'})
+
+
     return render(request, 'registration/login.html')
+
+@login_required(login_url='/login')
+def logout_view(request):
+    logout(request)
+    return redirect('login')
