d612c8660af2f90b7f3cc6b5d4bebe5254ea177c
gregorykariara1@gmail.com <gregorykariara1@gmail.com>
2024-08-22 23:10:26 +0300

    done for the day

--- admin/admin_app/urls.py
+++ admin/admin_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('home/', views.admin_view, name='admin'),
-    path('', RedirectView.as_view(url = 'home', permanent=True)),
+    path('', RedirectView.as_view(url = 'home/', permanent=True)),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('adminstrative_auth/users/', views.user_table, name='user_table'),
