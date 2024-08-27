1bfba97c62d6c4f749b67659ba5875bda2428d19
gregorykariara1@gmail.com <gregorykariara1@gmail.com>
2024-08-27 11:58:30 +0300

    Custom user and user photo

--- admin/admin_app/models.py
+++ admin/admin_app/models.py
@@ -1,5 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Permission, Group
 
 # Create your models here.
 class Messages(models.Model):
     message = models.CharField(max_length=255)
+
+class CustomUser(AbstractUser):
+    groups = models.ManyToManyField(
+        Group,
+        related_name='customuser_set',
+        blank=True,
+        help_text= ('This is the group a user belongs to and gets the permissions of that group.'),
+        related_query_name='user',
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='customuser_permissions_set',
+        blank=True,
+        related_query_name='user',
+        help_text=('Specific permissions of a user')
+    )
+    photo = models.ImageField(upload_to='user_photos/', blank=True, null=True)
